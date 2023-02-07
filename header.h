03efca15539567832af47f4735fd71a55bfb7e36
xueda3122356 <112776684+xueda3122356@users.noreply.github.com>
2023-02-07 11:34:36 -0800

    add header.h

---
+++ header.h
@@ -0,0 +1,17 @@
+
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+class header
+{
+	public:
+		void writeLine(string fileName);
+};
+
+#endif
